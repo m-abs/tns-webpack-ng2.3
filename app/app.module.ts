3964f0b21734d256e03c9e46162a178186dca284
Morten Sjøgren <m_abs@mabs.dk>
2017-01-14 20:59:51 +0100

    bundle error

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { NativeScriptModule } from "nativescript-angular/platform";
-
+import { NativeScriptModule } from 'nativescript-angular/platform';
 import { counterReducer, CounterEffects } from './store';
 
-import { AppComponent } from "./app.component";
-
+import { AppComponent } from './app.component';
 @NgModule({
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-    imports: [
-      NativeScriptModule,
-      StoreModule.provideStore({ counter: counterReducer }),
-      EffectsModule.run(CounterEffects),
-    ],
-    schemas: [NO_ERRORS_SCHEMA]
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [
+    NativeScriptModule,
+    StoreModule.provideStore({
+      counter: (counterReducer),
+    }),
+    EffectsModule.run(CounterEffects),
+  ],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }

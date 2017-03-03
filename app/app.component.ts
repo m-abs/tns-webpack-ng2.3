779323734eb28864b2f39fcadf7857a9252b01e4
Morten Sjøgren <m_abs@mabs.dk>
2017-03-03 14:02:51 +0100

    bug: ActionItems are reordered using *ngIf

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,17 +5,58 @@ import { Component } from "@angular/core";
     templateUrl: "app.component.html",
 })
 export class AppComponent {
-    public counter: number = 16;
+    public counter = 0;
+
+    private states = [
+      {
+        show1: true,
+        show2: true,
+      },
+      {
+        show1: false,
+        show2: true,
+      },
+      {
+        show1: true,
+        show2: true,
+      },
+      {
+        show1: true,
+        show2: false,
+      },
+      {
+        show1: true,
+        show2: true,
+      },
+      {
+        show1: false,
+        show2: false,
+      },
+    ];
+
+    public get show1() {
+      const idx = this.counter % this.states.length;
+      return this.states[idx].show1;
+    }
+
+    public get show2() {
+      const idx = this.counter % this.states.length;
+      return this.states[idx].show2;
+    }
 
     public get message(): string {
-        if (this.counter > 0) {
-            return this.counter + " taps left";
+        if (this.show1 && this.show2) {
+            return 'Show both ActionItems';
+        } else if (this.show1) {
+            return 'Show only "1"';
+        } else if (this.show2) {
+            return 'Show only "2"';
         } else {
-            return "Hoorraaay! \nYou are ready to start building!";
+            return `Show none\nNext should show in proper order`;
         }
     }
-    
+
     public onTap() {
-        this.counter--;
+        this.counter++;
     }
 }
